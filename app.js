e1866dbf94997f132b632d8bd1d89d92f294169b
bashamanth <bashamanth@gmail.com>
2018-11-15 09:27:08 +0530

    Data bindind S3L21

---
+++ app.js
@@ -0,0 +1,18 @@
+(function(){
+var com={}
+com.mydomain={}
+com.mydomain.Apropgram={}
+
+com.mydomain.Apropgram.utility=function utility()
+{
+    console.log("Programmer utility function");
+}
+
+com.mydomain.Apropgram.utility();
+
+function utility()
+{
+    console.log("Programmer ")
+}
+utility();
+}());
